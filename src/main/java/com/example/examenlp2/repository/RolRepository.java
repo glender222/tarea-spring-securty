b21da64d0eaac4cd8289c3047c68b79d25a74b11
glender333 <glender.vargasv@upeu.edu.pe>
2024-11-13 16:44:23 -0500

    tarea

---
+++ src/main/java/com/example/examenlp2/repository/RolRepository.java
@@ -0,0 +1,7 @@
+package com.example.examenlp2.repository;
+
+import com.example.examenlp2.entity.Rol;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RolRepository extends JpaRepository<Rol,Long> {
+}
---
+++ src/main/java/com/example/examenlp2/service/AuthService.java
@@ -0,0 +1,8 @@
+package com.example.examenlp2.service;
+
+
+import com.example.examenlp2.dto.LoginDto;
+
+public interface AuthService {
+    String login(LoginDto loginDto);
+}
